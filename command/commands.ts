5003d81b5bfdc01a71b75357c1958e3df2914ccd
listanegra <vitormac3@hotmail.com>
2020-12-29 22:11:09 -0300

    Basic bot implementation

---
+++ command/commands.ts
@@ -0,0 +1,7 @@
+import Command from './command.js'
+
+const play = new Command('play', async (client, message, args) => {
+
+});
+
+export default { play };
---
+++ index.ts
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+dotenv.config();
+
+import { Client } from 'discord.js'
+const client = new Client();
+
+const prefix = '$';
+const commands = require('./command/commands.js');
+
+client.on('ready', () => {
+    console.log('Bot is active');
+
+    client.on('message', message => {
+        if (message.channel.type === 'text' && message.content.startsWith(prefix)) {
+            const command = message.content.substr(1);
+            const args = command.split(/\s+/g);
+
+            if (args[0] in commands) {
+                commands[args[0]].executor(client, message, args);
+            }
+        }
+    });
+});
+
+client.login(process.env['TOKEN']);
