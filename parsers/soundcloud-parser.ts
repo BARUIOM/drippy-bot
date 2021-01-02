c365eea1b028247917dae942d9d15c5668f2bcf5
listanegra <vitormac3@hotmail.com>
2021-01-02 12:07:57 -0300

    SoundCloud error handling

--- parsers/soundcloud-parser.ts
+++ parsers/soundcloud-parser.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import cheerio from 'cheerio'
 
 import { Provider } from '../modules/parse-utils'
@@ -8,20 +8,23 @@ class SoundCloudParser implements MediaParser {
     async parse(url: string): Promise<Track[]> {
         const tracks: any[] = [];
 
-        const { data } = await axios.get(url);
-        const $ = cheerio.load(data);
+        return axios.get(url).then(({ data }) => {
+            const $ = cheerio.load(data);
 
-        const content = $('meta[property="al:ios:url"]')
-            .prop('content') as string;
+            const content = $('meta[property="al:ios:url"]')
+                .prop('content') as string;
 
-        if (content && content.length) {
-            const values = content.replace(/[/]/g, '')
+            const values = content.replace(/\//g, '')
                 .split(':');
+
+            if (!['sounds', 'playlists'].includes(values[1])) {
+                throw new Error('Invalid URL');
+            }
 
             const script = $('script').last().html() as string;
             const match = /\[\{.+\](?!.+\1)/gi.exec(script);
 
-            if (match !== null) {
+            if (match !== null && match[0]) {
                 const meta = JSON.parse(match[0]);
 
                 switch (values[1]) {
@@ -37,16 +40,22 @@ class SoundCloudParser implements MediaParser {
                         break;
                 }
             }
-        }
 
-        return tracks.map(({ id, title, permalink_url: href, artwork_url: thumbnail, user }) => ({
-            provider: Provider.SOUNDCLOUD,
-            id, title, href, thumbnail,
-            artists: [{
-                name: user.username,
-                href: user.permalink_url
-            }]
-        }));
+            return tracks.map(({ id, title, permalink_url: href, artwork_url: thumbnail, user }) => ({
+                provider: Provider.SOUNDCLOUD,
+                id, title, href, thumbnail,
+                artists: [{
+                    name: user.username,
+                    href: user.permalink_url
+                }]
+            }));
+        }).catch((e: AxiosError) => {
+            if (e.response) {
+                throw new Error('Invalid URL');
+            }
+
+            throw e;
+        });
     }
 
 }
