fe5e779c378fc5523b809f143bf4a6e59a3346aa
listanegra <vitormac3@hotmail.com>
2021-01-02 21:10:29 -0300

    Deezer error handling

--- parsers/deezer-parser.ts
+++ parsers/deezer-parser.ts
@@ -1,4 +1,4 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import cheerio from 'cheerio'
 
 import { Provider } from '../modules/parse-utils'
@@ -11,41 +11,52 @@ class DeezerParser implements MediaParser {
     async parse(url: string, params: string[]): Promise<Track[]> {
         const tracks: any[] = [];
 
-        const media_id = params.pop();
-        const media_type = params.pop();
+        const media_id = params.pop() as string;
+        const media_type = params.pop() as string;
 
-        const { data } = await axios.get(`https://deezer.com/${media_type}/${media_id}`);
-        const $ = cheerio.load(data);
-
-        const script = $('#naboo_content script')
-            .last().html() as string;
-        const match = /[{].+[}]/gi.exec(script);
-
-        if (match !== null && match[0]) {
-            const meta = JSON.parse(match[0]);
-
-            switch (media_type) {
-                case 'track':
-                    tracks.push(meta.DATA);
-                    break;
-                case 'album':
-                case 'playlist':
-                    meta.SONGS['data'].forEach((e: any) =>
-                        tracks.push(e)
-                    );
-                    break;
-            }
+        if (!['track', 'album', 'playlist'].includes(media_type)) {
+            throw new Error('Invalid URL');
         }
 
-        return tracks.map(({ SNG_ID: id, SNG_TITLE: title, ARTISTS, ALB_PICTURE }) => ({
-            provider: Provider.DEEZER,
-            id, title, href: `https://deezer.com/track/${id}`,
-            thumbnail: cover(ALB_PICTURE),
-            artists: ARTISTS.map((e: any) => ({
-                name: e.ART_NAME,
-                href: `https://deezer.com/artist/${e.ART_ID}`
-            }))
-        }));
+        return axios.get(`https://deezer.com/${media_type}/${media_id}`).then(({ data }) => {
+            const $ = cheerio.load(data);
+
+            const script = $('#naboo_content script')
+                .last().html() as string;
+            const match = /[{].+[}]/gi.exec(script);
+
+            if (match !== null && match[0]) {
+                const meta = JSON.parse(match[0]);
+
+                switch (media_type) {
+                    case 'track':
+                        tracks.push(meta.DATA);
+                        break;
+                    case 'album':
+                    case 'playlist':
+                        meta.SONGS['data'].forEach((e: any) =>
+                            tracks.push(e)
+                        );
+                        break;
+                }
+            }
+
+            return tracks.map(({ SNG_ID: id, SNG_TITLE: title, ARTISTS, ALB_PICTURE }) => ({
+                provider: Provider.DEEZER,
+                id, title, href: `https://deezer.com/track/${id}`,
+                thumbnail: cover(ALB_PICTURE),
+                artists: ARTISTS.map((e: any) => ({
+                    name: e.ART_NAME,
+                    href: `https://deezer.com/artist/${e.ART_ID}`
+                }))
+            }));
+        }).catch((e: AxiosError) => {
+            if (e.response) {
+                throw new Error('Invalid URL');
+            }
+
+            throw e;
+        });
     }
 
 }
