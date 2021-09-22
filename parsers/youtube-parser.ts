193f0855a4ae19d86eee570470b64ff36e543d2d
listanegra <vitormac3@hotmail.com>
2021-09-22 19:48:38 -0300

    Youtube URL parser refactor

--- parsers/youtube-parser.ts
+++ parsers/youtube-parser.ts
@@ -2,33 +2,65 @@ import { Source, YoutubeApi } from 'search-api-core';
 
 import { Fetcher } from '../modules/track-fetcher';
 
+const _error = (message: string = 'Invalid Youtube URL') => new Error(message);
+
+const _validPath = (url: URL): boolean =>
+    (url.hostname === 'youtu.be' && !!url.pathname.substr(1)) || ['embed', 'watch', 'playlist']
+        .some(e => url.pathname.startsWith('/' + e));
+
 class YoutubeParser implements MediaParser {
 
-    async parse(url: string, params: string[]): Promise<Track[]> {
-        if (params.length < 2 || !['embed', 'watch', 'playlist'].includes(params[0])) {
-            throw new Error('Invalid URL');
+    async parse(url: URL): Promise<Track[]> {
+        if (!_validPath(url)) {
+            throw _error();
         }
 
-        if (params[0] === 'playlist') {
-            const list = params.pop() as string;
+        if (url.pathname.startsWith('/playlist')) {
+            const list = url.searchParams.get('list');
 
-            return Fetcher.YoutubeClient.getPlaylist(list, YoutubeApi.ClientType.YOUTUBE)
-                .then(playlist =>
-                    playlist.contents.map(video => ({
+            if (!list)
+                throw _error();
+
+            if (url.hostname.startsWith('music')) {
+                return Fetcher.YoutubeClient.getPlaylist(
+                    list, YoutubeApi.ClientType.YOUTUBE_MUSIC
+                ).then(playlist =>
+                    playlist.contents.map(music => ({
                         provider: Source.YOUTUBE_MUSIC,
-                        id: video.id,
-                        title: video.title,
-                        href: video.href,
-                        thumbnail: video.thumbnails[0].href,
-                        artists: [{
-                            name: video.channel.name,
-                            href: video.channel.href
-                        }]
+                        id: music.id,
+                        title: music.title,
+                        href: music.href,
+                        thumbnail: music.thumbnails[0].href,
+                        artists: music.artists
                     }))
                 );
+            }
+
+            return Fetcher.YoutubeClient.getPlaylist(
+                list, YoutubeApi.ClientType.YOUTUBE
+            ).then(playlist =>
+                playlist.contents.map(video => ({
+                    provider: Source.YOUTUBE_MUSIC,
+                    id: video.id,
+                    title: video.title,
+                    href: video.href,
+                    thumbnail: video.thumbnails[0].href,
+                    artists: [{
+                        name: video.channel.name,
+                        href: video.channel.href
+                    }]
+                }))
+            );
         }
 
-        const video_id = params.pop() as string;
+        if (url.hostname === 'youtu.be') {
+            url.searchParams.set('v', url.pathname.substr(1));
+        }
+
+        const video_id = url.searchParams.get('v');
+
+        if (!video_id)
+            throw _error();
 
         return Fetcher.YoutubeClient.getVideo(video_id)
             .then(video => {
